cba01efbd7916f4ad508b9d900bf72f191cf2263
Talha <talharizvi480@gmail.com>
2019-06-24 16:01:58 +0530

    implemented navigation

--- NextScreen.js
+++ NextScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text,Button } from 'react-native';
-// import { createStackNavigator, createAppContainer } from "react-navigation";
+
 
 class NextScreen extends React.Component{
     render(){
